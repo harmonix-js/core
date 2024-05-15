7115f743c50e15ce637c55f2be78ef8d0dc81d7a
nethris <david.abou@epitech.eu>
2024-05-15 10:18:10 -0400

    test: playground

--- playground/commands/utils/noop.ts
+++ playground/commands/utils/noop.ts
@@ -1,8 +1,29 @@
-import { defineCommand } from '../../../src'
+import { ChannelType } from 'discord.js'
+import { defineArgument, defineCommand } from '../../../src'
 
-export default defineCommand<false>(
+export default defineCommand(
   {
+    slash: true,
     description: 'NOOP!',
+    args: [
+      defineArgument({
+        name: 'noop',
+        type: 'Integer',
+        description: 'NOOP!',
+        metadata: {
+          choices: [
+            {
+              name: '1',
+              value: 1
+            },
+            {
+              name: '2',
+              value: 2
+            }
+          ]
+        }
+      })
+    ],
     preconditions: ['nsfw']
   },
   (_, message) => {
--- playground/harmonix.config.ts
+++ playground/harmonix.config.ts
@@ -1,6 +1,5 @@
 import { defineHarmonixConfig } from '../src'
 
 export default defineHarmonixConfig({
-  clientId: '929023549656662047',
   ownerId: ['556083802628161546']
 })
