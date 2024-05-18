8f504f7e1c19521eb322623e5a0d58d02b928b57
nethris <david.abou@epitech.eu>
2024-05-18 14:44:05 -0400

    test: playground

--- playground/harmonix.config.ts
+++ playground/harmonix.config.ts
@@ -1,6 +1,5 @@
 import { defineHarmonixConfig } from '../src'
 
 export default defineHarmonixConfig({
-  ownerId: ['556083802628161546'],
-  guildId: ['725902714311278643']
+  ownerIds: ['556083802628161546']
 })
